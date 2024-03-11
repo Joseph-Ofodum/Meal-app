c1f8eb6761e3f1d3f9a4b8ea723df1cea34552fa
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-03-11 07:21:47 +0100

    added ingredients and steps in the meal details class made it scrollable with SinglechildScrowView widget

--- lib/widgets/meal_details.dart
+++ lib/widgets/meal_details.dart
@@ -1,26 +1,69 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:meal/models/meal.dart';
 
-class MealDetailsScreen extends StatelessWidget{
-  const MealDetailsScreen ({super.key, required this.meal,});
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
 
   final Meal meal;
 
-@override
+  @override
   Widget build(BuildContext context) {
- return Scaffold(
-  appBar: AppBar(
-    title: Text(meal.title),
-    ),
-
-    body: Image.network(meal.imageUrl,
-    height: 300,
-    width: double.infinity,
-    fit: BoxFit.cover,
-    ) ,
- );
-
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14,),
+              for (final ingredient in meal.ingredients)
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+                const SizedBox(height: 24,),
+                Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14,),
+              for (final step in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    step,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        ));
   }
 }
