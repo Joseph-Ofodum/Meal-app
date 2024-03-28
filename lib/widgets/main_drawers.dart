f44a78a78c57c3347209104c7de0fb81c6d55fe0
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-03-28 09:57:51 +0100

    closing the drawer manually with navigator and pop

--- lib/widgets/main_drawers.dart
+++ lib/widgets/main_drawers.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +37,7 @@ class MainDrawer extends StatelessWidget {
                   ),
                 ],
               )),
-          ListTile(
+          ListTile(  
             leading: Icon(
               Icons.restaurant,
               size: 26,
@@ -47,7 +49,9 @@ class MainDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -61,7 +65,9 @@ class MainDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('filters');
+            },
           )
         ],
       ),
